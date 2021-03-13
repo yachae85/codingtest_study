26a213cc8ce17550106c79a95ae92f8d8aa5297b
김용후 <dydgndlaek97@gmail.com>
2021-03-13 19:24:54 +0900

    Update 2_중복문자제거_yonghu.py

--- 1주차/2_중복문자제거_yonghu.py
+++ 1주차/2_중복문자제거_yonghu.py
@@ -6,14 +6,14 @@ class Solution:
         stack = []
         count = collections.Counter(s)
         seen = set()
-        for char in s:
-            count[char] -= 1
-            if char in seen:
+        for c in s:
+            count[c] -= 1
+            if c in seen:
                 continue
 
             while stack and stack[-1] > char and count[stack[-1]] > 0:
                 seen.remove(stack.pop())
 
-            stack.append(char)
-            seen.add(char)
+            stack.append(c)
+            seen.add(c)
         return ''.join(stack)
