dced6f4a476246ed2c0be4b9f860de691dc64225
김용후 <dydgndlaek97@gmail.com>
2021-03-23 00:16:43 +0900

    통과하게 수정했슴

--- 2주차/용후/17135_캐슬디펜스.py
+++ 2주차/용후/17135_캐슬디펜스.py
@@ -1,8 +1,9 @@
+# boj g4
 from collections import deque
 import sys
 
 n, m, d = map(int, input().split())
-field = [list(map(int, input().split())) for _ in range(n)]
+origin_field = [list(map(int, input().split())) for _ in range(n)]
 archer_combination = []
 
 dx = [0, -1, 0]
@@ -22,54 +23,49 @@ def get_distance(r1, c1, r2, c2):
     return abs(r1-r2) + abs(c1-c2)
 
 
-def bfs(archer, field):
-    x, y = 0, 1
+def attack(archer, field, dead):
     q = deque()
     visited = [[False for __ in range(m)] for _ in range(n)]
     q.append((n-1, archer))
-    kill = 0
 
     while q:
         pos = q.popleft()
+        x = pos[0]
+        y = pos[1]
+        visited[x][y] = True
 
-        visited[pos[x]][pos[y]] = True
-        if field[pos[x]][pos[y]] == 1:
-            kill += 1
-            field[pos[x]][pos[y]] = 0
-            break
+        if field[x][y] == 1:
+            dead.add((x, y))
+            return
 
         for i in range(3):
-            tx = pos[x] + dx[i]
-            ty = pos[y] + dy[i]
+            tx = x + dx[i]
+            ty = y + dy[i]
 
             if tx < 0 or ty < 0 or ty >= m:
                 continue
+            if visited[tx][ty]:
+                continue
             if get_distance(n, archer, tx, ty) > d:
                 continue
-            if not visited[tx][ty]:
-                q.append((tx, ty))
-
-    return kill
+            q.append((tx, ty))
 
 
 get_archer_combination([], 0, 0)
-
 result = -sys.maxsize
-first_enemy = sum(sum(row) for row in field)
-for archers in archer_combination:
-    enemy = first_enemy
-    kill = 0
-    cp = [row[:] for row in field]
-    while enemy > 0:
-        # attack
-        for archer in archers:
-            k = bfs(archer, cp)
-            enemy -= k
-            kill += k
-        # move_down
-        cp.insert(0, [0 for _ in range(m)])
-        eleminated = cp.pop()
-        enemy -= eleminated.count(1)
-    result = max(kill, result)
 
+for archers in archer_combination:
+    kill = 0
+    field = [row[:] for row in origin_field]
+
+    for _ in range(n):
+        dead = set()
+        for archer in archers:
+            attack(archer, field, dead)
+        for ex, ey in dead:
+            kill += 1
+            field[ex][ey] = 0
+        field.pop()
+        field.insert(0, [0]*m)
+    result = max(kill, result)
 print(result)
